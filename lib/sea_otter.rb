f842208be3985b7c08f3e8ece5b735168034e922
alexzicat <dev.alexzicat@gmail.com>
2017-12-14 10:33:28 -0500

    fix on_load hook

--- lib/sea_otter.rb
+++ lib/sea_otter.rb
@@ -5,6 +5,13 @@ require 'sea_otter/js_context/base'
 require 'sea_otter/js_context/error'
 require 'sea_otter/version'
 
-ActiveSupport.on_load(:action_view) {
-  include SeaOtter::ApplicationHelper
-}
+module SeaOtter
+  class Railtie < Rails::Railtie
+
+    initializer 'adds sea_otter view helpers' do
+      ActiveSupport.on_load(:action_view) {
+        include SeaOtter::ApplicationHelper
+      }
+    end
+  end
+end
