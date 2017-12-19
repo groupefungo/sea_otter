92ec3ebf1843ed8cb7ab0b05af76a96204fec183
alexzicat <dev.alexzicat@gmail.com>
2017-12-19 14:22:18 -0500

    fix json props

--- app/helpers/sea_otter/application_helper.rb
+++ app/helpers/sea_otter/application_helper.rb
@@ -4,21 +4,19 @@ module SeaOtter
     def render_react_app(props = {})
       @react_server_exports = {}
 
-      json_props = props.to_json
-
       @react_server_exports = SeaOtter::Renderer::Base.render(props: props)
 
       content_for(SeaOtter.configuration.content_for_name) do
         [
-            SeaOtter::Renderer::Base.print_preloaded_state(json_props),
+            SeaOtter::Renderer::Base.print_preloaded_state(props),
             SeaOtter::Renderer::Base.print_console_logs(@react_server_exports['logs']),
-            SeaOtter::Renderer::Base.set_preloaded_state(json_props),
+            SeaOtter::Renderer::Base.set_preloaded_state(props),
         ].join("\n").html_safe
       end
 
       @react_server_exports['html']&.html_safe
     rescue MiniRacer::RuntimeError => error
-      renderer_error = SeaOtter::Renderer::Error.new(error, json_props, SeaOtter.configuration.server_bundle_path)
+      renderer_error = SeaOtter::Renderer::Error.new(error, props.to_json, SeaOtter.configuration.server_bundle_path)
 
       render(partial: '/sea_otter/errors', locals: {error: renderer_error})
     end
